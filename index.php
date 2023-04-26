dfa832e43ad16684789eb15562c428e34a97ad44
Fami6Xx <libojer@gmail.com>
2023-04-27 00:38:40 +0200

    feat: categories

--- index.php
+++ index.php
@@ -11,16 +11,67 @@
 <body>
 <?php include "navbar.php"; ?>
 
-<div class="col-md-6 col-lg-4 my-3">
-    <div class="card">
-        <div class="card-header"><h5 class="card-title">Post Title</h5></div>
-        <div class="card-body">
-            <h6 class="card-subtitle mb-2 text-muted">Author: Your mother</h6>
-            <p class="card-subtitle mb-2 text-muted">Date: 4.20.0000</p>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur delectus, dolorem harum iure maxime similique voluptatibus? Ad aliquid dolorem eius fugiat hic maiores maxime molestiae, natus, necessitatibus nesciunt nostrum, odio officiis omnis praesentium quisquam sunt veniam veritatis voluptas? Ea?</p>
-            <a href="post_detail.php" class="card-link">Read More</a>
-        </div>
-        <div class="card-footer">Last updated 3 mins ago</div>
+<div class="container my-4">
+    <h2 class="text-center mb-4">Category 1</h2>
+    <div class="row justify-content-center">
+        <?php for ($i = 1; $i <= 3; $i++): ?>
+            <div class="col-md-6 col-lg-4 my-3">
+                <div class="card">
+                    <div class="card-header"><h5 class="card-title">Post Title <?= $i; ?></h5></div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted">Author: Your mother</h6>
+                        <p class="card-subtitle mb-2 text-muted">Date: 4.20.0000</p>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur delectus, dolorem harum iure maxime similique voluptatibus? Ad aliquid dolorem eius fugiat hic maiores maxime molestiae, natus, necessitatibus nesciunt nostrum, odio officiis omnis praesentium quisquam sunt veniam veritatis voluptas? Ea?</p>
+                        <a href="post_detail.php" class="card-link">Read More</a>
+                    </div>
+                    <div class="card-footer"><small>Last updated 3 mins ago</small></div>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </div>
+
+    <br/>
+    <hr/>
+    <br/>
+
+    <h2 class="text-center mb-4">Category 2</h2>
+    <div class="row justify-content-center">
+        <?php for ($i = 1; $i <= 3; $i++): ?>
+            <div class="col-md-6 col-lg-4 my-3">
+                <div class="card">
+                    <div class="card-header"><h5 class="card-title">Post Title <?= $i; ?></h5></div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted">Author: Your mother</h6>
+                        <p class="card-subtitle mb-2 text-muted">Date: 4.20.0000</p>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur delectus, dolorem harum iure maxime similique voluptatibus? Ad aliquid dolorem eius fugiat hic maiores maxime molestiae, natus, necessitatibus nesciunt nostrum, odio officiis omnis praesentium quisquam sunt veniam veritatis voluptas? Ea?</p>
+                        <a href="post_detail.php" class="card-link">Read More</a>
+                    </div>
+                    <div class="card-footer"><small>Last updated 3 mins ago</small></div>
+                </div>
+            </div>
+        <?php endfor; ?>
+    </div>
+
+    <br/>
+    <hr/>
+    <br/>
+
+    <h2 class="text-center mb-4">Category 3</h2>
+    <div class="row justify-content-center">
+        <?php for ($i = 1; $i <= 3; $i++): ?>
+            <div class="col-md-6 col-lg-4 my-3">
+                <div class="card">
+                    <div class="card-header"><h5 class="card-title">Post Title <?= $i; ?></h5></div>
+                    <div class="card-body">
+                        <h6 class="card-subtitle mb-2 text-muted">Author: Your mother</h6>
+                        <p class="card-subtitle mb-2 text-muted">Date: 4.20.0000</p>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur delectus, dolorem harum iure maxime similique voluptatibus? Ad aliquid dolorem eius fugiat hic maiores maxime molestiae, natus, necessitatibus nesciunt nostrum, odio officiis omnis praesentium quisquam sunt veniam veritatis voluptas? Ea?</p>
+                        <a href="post_detail.php" class="card-link">Read More</a>
+                    </div>
+                    <div class="card-footer"><small>Last updated 3 mins ago</small></div>
+                </div>
+            </div>
+        <?php endfor; ?>
     </div>
 </div>
 <script src="js/bootstrap.js"></script>
