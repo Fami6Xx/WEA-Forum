03d7b98be458c680374ec9c8f52f7236cd49741e
Fami6Xx <libojer@gmail.com>
2023-04-27 00:13:08 +0200

    feat: navbar

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="js/bootstrap.js"></script>
     <link rel="stylesheet" href="css/bootstrap.css">
-    <title>Document</title>
+    <title>Forum</title>
 </head>
 <body>
+<?php include "navbar.php"; ?>
 
+<script src="js/bootstrap.js"></script>
 </body>
 </html>
