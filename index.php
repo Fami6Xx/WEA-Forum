cdb64f3689d06cfd09773d3cf93add1aeefb413d
Fami6Xx <libojer@gmail.com>
2023-04-27 00:28:10 +0200

    feat: card

--- index.php
+++ index.php
@@ -11,6 +11,18 @@
 <body>
 <?php include "navbar.php"; ?>
 
+<div class="col-md-6 col-lg-4 my-3">
+    <div class="card">
+        <div class="card-header"><h5 class="card-title">Post Title</h5></div>
+        <div class="card-body">
+            <h6 class="card-subtitle mb-2 text-muted">Author: Your mother</h6>
+            <p class="card-subtitle mb-2 text-muted">Date: 4.20.0000</p>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque consectetur delectus, dolorem harum iure maxime similique voluptatibus? Ad aliquid dolorem eius fugiat hic maiores maxime molestiae, natus, necessitatibus nesciunt nostrum, odio officiis omnis praesentium quisquam sunt veniam veritatis voluptas? Ea?</p>
+            <a href="post_detail.php" class="card-link">Read More</a>
+        </div>
+        <div class="card-footer">Last updated 3 mins ago</div>
+    </div>
+</div>
 <script src="js/bootstrap.js"></script>
 </body>
 </html>
