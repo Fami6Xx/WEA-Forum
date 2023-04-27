f28ca6ebfc178894d9c6579d312f359412e18dbe
Fami6Xx <libojer@gmail.com>
2023-04-27 02:04:03 +0200

    feat: added a href to forgot password

--- login.php
+++ login.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/login.css">
     <title>Login</title>
 </head>
@@ -22,6 +23,9 @@
 
     <button>Log In</button>
 
+    <div class="text-center mt-3">
+        <a href="forgot_password.php">Zapomenuté heslo</a>
+    </div>
 </form>
 </body>
 </html>
