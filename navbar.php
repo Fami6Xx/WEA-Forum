3c99d5c07ed58ca89e7f5376328a1d2c950cb33c
Fami6Xx <libojer@gmail.com>
2023-04-28 20:33:43 +0200

    refactor: simplified navbar

--- navbar.php
+++ navbar.php
@@ -1,14 +1,12 @@
-
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
+    <div class="container d-flex justify-content-center">
         <a class="navbar-brand" href="index.php">Forum</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMain" aria-controls="navbarMain" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
         <div class="collapse navbar-collapse" id="navbarMain">
-            <ul class="navbar-nav me-auto">
+            <ul class="navbar-nav">
                 <li class="nav-item active">
                     <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
                 </li>
@@ -25,9 +23,6 @@
                     </li>
                 <?php endif; ?>
             </ul>
-            <form class="form-inline my-2 my-md-0">
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-            </form>
         </div>
     </div>
 </nav>
