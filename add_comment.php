e72700c4a4ab89d4a8e389fe67fe3e54510277ae
Fami6Xx <libojer@gmail.com>
2023-04-28 20:18:50 +0200

    feat: add_comment now adds one to num_comments of post

--- add_comment.php
+++ add_comment.php
@@ -12,5 +12,11 @@
     }
     $stmt->bind_param("ssss", $post_id, $author, $content, $date);
     $stmt->execute();
+
+    $sql_update = "UPDATE posts SET num_comments = num_comments + 1 WHERE id = ?";
+    $stmt_update = $conn->prepare($sql_update);
+    $stmt_update->bind_param("s", $post_id);
+    $stmt_update->execute();
+
     header('Location: ' . $_SERVER['HTTP_REFERER']);
 ?>
