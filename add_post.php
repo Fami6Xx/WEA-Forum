655d4d5d2477f1652c304b7a4fb278f6c024f3e8
Fami6Xx <libojer@gmail.com>
2023-04-28 19:30:52 +0200

    fix: server fix for creating posts when not logged in

--- add_post.php
+++ add_post.php
@@ -1,16 +1,19 @@
 <?php
     var_dump($_POST);
     session_start();
+    require_once("config.php");
+    $sql = "INSERT INTO posts (title, author, date, content) VALUES (?, ?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $date = date("Y-m-d H:i:s");
     if($_SESSION && $_SESSION['loggedin']){
-        require_once("config.php");
-        $sql = "INSERT INTO posts (title, author, date, content) VALUES (?, ?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $title = $_POST['title'];
-        $content = $_POST['content'];
-        $date = date("Y-m-d H:i:s");
-        $author = "".$_SESSION['accountdetails']['fistname']." ".$_SESSION['accountdetails']['lastname']."";
-        $stmt->bind_param("ssss", $title, $author, $date, $content);
-        $stmt->execute();
-        header("Location: index.php");
+        $author = "".$_SESSION['accountdetails']['username'];
+    }else{
+        $author = "Neregistrovaný uživatel";
     }
+
+    $stmt->bind_param("ssss", $title, $author, $date, $content);
+    $stmt->execute();
+    header("Location: index.php");
 ?>
